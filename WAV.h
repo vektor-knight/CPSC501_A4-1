78695e2a5474c71652b07cd1a1d52ef8b897ba51
shshunda <shshunda@csx3.cs.ucalgary.ca>
2016-12-08 18:40:51 -0700

    Created WAVE header

---
+++ WAV.h
@@ -0,0 +1,34 @@
+#ifndef WAV
+#define WAV
+
+using namespace std;
+
+class WAV
+{
+	public:
+		// Canonical WAVE file format
+		// Reference: https://i.stack.imgur.com/ITplE.gif
+
+		// Segment 1:  "RIFF" chunk descriptor 
+		char *chunkID;
+		int chunkSize;
+		char *format;
+		// Segment 2: "fmt" sub-chunk
+		char *subChunk1ID;
+		int sucChunk1Size;
+		int16_t audioFormat;
+		int16_t numChannels;
+		int sampleRate;
+		int byteRate;
+		int16_t blockAlign;
+		int16_t bitsPerSample;
+		// Segment 3: "data" sub-chunk
+		char *subChunk2ID;
+		int subChunk2Size;
+		int dataSize;
+
+		// Helper function prototypes: read/write
+		// Reference: https://github.com/rsbarhey/CPSC501-A4/blob/master/V2.0/WaveFile.h
+		float* readWAV(char *fName, float *signal, int *size);
+		void writeWAV(char* fName, int numSamples, float *size);
+};
